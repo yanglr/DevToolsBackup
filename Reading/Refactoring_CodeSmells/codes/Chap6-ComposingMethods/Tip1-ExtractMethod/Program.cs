865d43a58b436382d0f03880403b0560ed8af105
yanglr <yangle20081982@163.com>
2024-09-21 17:40:13 +0800

    Adjust chap 6 projects.

--- Reading/Refactoring_CodeSmells/codes/Chap6-ComposingMethods/Tip1-ExtractMethod/Program.cs
+++ Reading/Refactoring_CodeSmells/codes/Chap6-ComposingMethods/Tip1-ExtractMethod/Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.ReadKey();
     }
 }
