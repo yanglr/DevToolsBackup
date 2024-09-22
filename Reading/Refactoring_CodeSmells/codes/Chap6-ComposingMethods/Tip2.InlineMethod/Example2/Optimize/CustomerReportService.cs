fbcb8c84ee9762dd5e3c5efd05869f0d696153bd
yanglr <yangle20081982@163.com>
2024-09-22 22:59:48 +0800

    Add optimize version for chap 6 - Tip2.

---
+++ Reading/Refactoring_CodeSmells/codes/Chap6-ComposingMethods/Tip2.InlineMethod/Example2/Optimize/CustomerReportService.cs
@@ -0,0 +1,14 @@
+﻿namespace Tip2.InlineMethod.Example2.Optimize
+{
+    public class CustomerReportService
+    {
+        public List<Tuple<string, string>> ReportLines(Customer customer)
+        {
+            return new List<Tuple<string, string>>
+            {
+                Tuple.Create("name", customer.Name),
+                Tuple.Create("location", customer.Location)
+            };
+        }
+    }
+}
