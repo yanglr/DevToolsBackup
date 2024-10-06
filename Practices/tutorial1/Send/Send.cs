f29d404e66d8f74c4f668aa736d88e12eb39ac1c
yanglr <yangle20081982@163.com>
2024-10-06 22:17:35 +0800

    Add C# tutorial1 for RabbitMQ.

---
+++ Practices/tutorial1/Send/Send.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    internal class Send
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            Console.WriteLine("Please enter the message you want to send:");
+            string? message = Console.ReadLine();
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                             routingKey: "hello",
+                             basicProperties: null,
+                             body: body);
+            Console.WriteLine($" [x] Sent: {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
