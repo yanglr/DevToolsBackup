d373f7903c70125d861c1f93dfdc9921e2544962
yanglr <yangle20081982@163.com>
2024-11-06 22:54:07 +0800

    Add code for Tip8.IntroduceLocalExtension.

---
+++ Reading/Refactoring_CodeSmells/codes/Chap7-MovingFeaturesBetweenObjects/Tip8.IntroduceLocalExtension/1.Inheritance/CustomDateTime.cs
@@ -0,0 +1,17 @@
+﻿using Tip8.IntroduceLocalExtension;
+
+namespace Tip8.IntroduceLocalExtension1.Inheritance
+{
+    internal class CustomDateTime : OriginalDate
+    {
+        public CustomDateTime(DateTime date) : base(date)
+        {
+        }
+
+        public bool IsHoliday()
+        {
+            // Check if the date is a holiday
+            return false;
+        }
+    }
+}
---
+++ Reading/Refactoring_CodeSmells/codes/Chap7-MovingFeaturesBetweenObjects/Tip8.IntroduceLocalExtension/3.ExtensionClass/OriginalDateExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Tip8.IntroduceLocalExtension;
+
+namespace Tip8.IntroduceLocalExtension_3.ExtensionClass
+{
+    internal static class OriginalDateExtensions
+    {
+        public static bool IsHoliday(this OriginalDate originalDate)
+        {
+            // Check if the date is a holiday
+            return false;
+        }
+    }
+}
