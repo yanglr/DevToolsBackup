eb7ae158abea43372f5cb53d114b049da0a2ad48
yanglr <yangle20081982@163.com>
2024-12-01 20:29:59 +0800

    Add Step4 for Tip04.SeparateQueryfromModifier.

---
+++ Reading/Refactoring_CodeSmells/codes/Chap10-MakingMethodCallsSimpler/Tip04.SeparateQueryfromModifier/Step4/NameFinder.cs
@@ -0,0 +1,44 @@
+﻿namespace Tip04.SeparateQueryfromModifier.Step4
+{
+    internal class NameFinder
+    {
+        public void CheckSecurity(string[] people)
+        {
+            AlertForMiscreant(people);
+            string foundName = FoundPerson(people);
+            FurtherProcess(foundName);
+        }
+
+        internal void AlertForMiscreant(string[] people)
+        {
+            if (!string.IsNullOrEmpty(FoundPerson(people)))
+            {
+                SendAlert();
+            };
+        }
+
+        private string FoundPerson(string[] people)
+        {
+            for (int i = 0; i < people.Length; i++)
+            {
+                string name = people[i];
+                if (people[i].Equals("Don") || people[i].Equals("John"))
+                {
+                    return name;
+                }
+            }
+
+            return "";
+        }
+
+        private void SendAlert()
+        {
+            Console.WriteLine("The miscreant is found.");
+        }
+
+        private void FurtherProcess(string foundName)
+        {
+            Console.WriteLine($"Then length of {foundName} is: {foundName.Length}");
+        }
+    }
+}
---
+++ Reading/Refactoring_CodeSmells/codes/Chap10-MakingMethodCallsSimpler/Tip04.SeparateQueryfromModifier/Step4/TestClient.cs
@@ -0,0 +1,14 @@
+﻿namespace Tip04.SeparateQueryfromModifier.Step4
+{
+    internal class TestClient
+    {
+        public static void Test()
+        {
+            var nameFinder = new NameFinder();
+            var people = new string[] { "John", "Andy", "Kent" };
+            nameFinder.CheckSecurity(people);
+
+            Console.ReadKey();
+        }
+    }
+}
